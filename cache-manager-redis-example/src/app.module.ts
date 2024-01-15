3cf6a989836c57c1bc4588ea5a7050c84d3f524e
Livinus Igbaji <bestbrain10@gmail.com>
2024-01-15 11:11:45 +0100

    add service example

--- cache-manager-redis-example/src/app.module.ts
+++ cache-manager-redis-example/src/app.module.ts
@@ -1,9 +1,34 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { CacheModule } from '@nestjs/cache-manager';
+import { redisStore } from 'cache-manager-redis-store';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [
+        () => ({
+          REDIS_URI: 'Get from environmental variables maybe or raw value here',
+        }),
+      ],
+    }),
+    CacheModule.registerAsync<any>({
+      isGlobal: true,
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => {
+        const store = await redisStore({
+          url: configService.get('REDIS_URI'),
+        });
+        return {
+          store: () => store,
+        };
+      },
+      inject: [ConfigService],
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- cache-manager-redis-example/src/app.service.ts
+++ cache-manager-redis-example/src/app.service.ts
@@ -1,8 +1,24 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
+import { CACHE_MANAGER } from '@nestjs/cache-manager';
+import { Cache } from 'cache-manager';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  constructor(
+    @Inject(CACHE_MANAGER)
+    private readonly cacheManager: Cache,
+  ) {}
+
+  async getHello(): Promise<string> {
+    const key = 'helloKey';
+    const cachedValue = await this.cacheManager.get<string>(key);
+
+    if (!cachedValue) {
+      const value = 'Hello World!';
+      await this.cacheManager.set(key, value, 1000);
+      return value;
+    }
+
+    return cachedValue;
   }
 }
